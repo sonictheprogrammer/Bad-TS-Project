c295ffab72b6bcf3c71f5a485120e44016dafeb6
sonictheprogrammer <92324146+sonictheprogrammer@users.noreply.github.com>
2021-12-01 15:41:32 -0500

    bad ts code here

---
+++ TypeFardCode.ts
@@ -0,0 +1,23 @@
+console.log("fard")
+var fard = true {
+    let fard = "brrr";
+}
+let fard: string
+var string = true
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+function logPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+}
+
+// logs "12, 26"
+const point = { x: 12, y: 26 };
+logPoint(point);
+
+var logpoint = true
+
+let logpoint: const user: User = new UserAccount("Helloworld", 1)
