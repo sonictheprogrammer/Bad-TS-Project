52d1a6ed063f5963dc8754272b0f435e9cf621cb
sonictheprogrammer <92324146+sonictheprogrammer@users.noreply.github.com>
2021-12-04 10:18:31 -0500

    i hope this java files work this time.

---
+++ NewJacupFile.java
@@ -0,0 +1,10 @@
+import Jacup
+
+public class ERROR {
+    pubilc static  void(String[]  args) {
+
+        print(boolean b)
+
+        pubilc void println(String s)
+
+        out.format(format, args)
